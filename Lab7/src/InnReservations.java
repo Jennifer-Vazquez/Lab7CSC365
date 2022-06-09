2ea522f4c1002e8ead9368023d21987c8a4c108c
jennifer-vazquez <vazquez.jennifer5@gmail.com>
2022-06-09 14:55:02 -0700

    2 is almost done

--- Lab7/src/InnReservations.java
+++ Lab7/src/InnReservations.java
@@ -9,6 +9,17 @@ import java.util.Scanner;
 import java.time.LocalDate;
 
 public class InnReservations {
+    public static class PossibleEntries {
+        public String RoomCode, RoomN;
+        public int rate;
+
+            // constructor
+            public PossibleEntries(String RC, String RN, int Ra) {
+                this.RoomCode = RC;
+                this.RoomN = RN;
+                this.rate = Ra;
+            }
+    }
 
     public static HashMap<String, String> LinksToInputs = new HashMap<String, String>();
 
@@ -19,6 +30,8 @@ public class InnReservations {
         LinksToInputs.put("D", "Room");
         LinksToInputs.put("E", "Code");
 
+        String EnvURL = System.getenv("HP_JDBC_URL");
+        System.out.println(EnvURL);
         try (Connection conn = DriverManager.getConnection(System.getenv("HP_JDBC_URL"),
                 System.getenv("HP_JDBC_USER"),
                 System.getenv("HP_JDBC_PW"))) {
@@ -33,6 +46,9 @@ public class InnReservations {
                 printOptions();
                 System.out.print("Input: ");
                 optionSelected = myScan.nextLine();
+                if (optionSelected.equals("2")) {
+                    reservationSearch(myScan, conn);
+                }
                 if (optionSelected.equals("3")) {
                     reservationChange(myScan, conn);
                 }
@@ -155,7 +171,6 @@ public class InnReservations {
         }
     }
 
-
     public static void deleteReservation(Scanner scanner, Connection conn) {
 
         int reservationCode;
@@ -183,7 +198,6 @@ public class InnReservations {
         }
     }
 
-    //First name•Last name•A range of dates•Room code•Reservation code
     public static void detailedReservation(Scanner scanner, Connection conn) throws SQLException {
         String arg1Option;
         String arg1 = "";
@@ -263,13 +277,12 @@ public class InnReservations {
                         sql_stm.append("%' and ");
                     } else {
                         sql_stm.append(LinksToInputs.get(arg1Option));
-                        if(arg1Option.equals("D")){
+                        if (arg1Option.equals("D")) {
                             sql_stm.append(" = '");
                             sql_stm.append(arg1);
                             sql_stm.append("'");
                             sql_stm.append(" and ");
-                        }
-                        else {
+                        } else {
                             sql_stm.append(" = ");
                             sql_stm.append(arg1);
                             sql_stm.append(" and ");
@@ -282,12 +295,11 @@ public class InnReservations {
                         sql_stm.append("%'");
                     } else {
                         sql_stm.append(LinksToInputs.get(arg2Option));
-                        if(arg2Option.equals("D")){
+                        if (arg2Option.equals("D")) {
                             sql_stm.append(" = '");
                             sql_stm.append(arg2);
                             sql_stm.append("'");
-                        }
-                        else {
+                        } else {
                             sql_stm.append(" = ");
                             sql_stm.append(arg2);
                         }
@@ -301,12 +313,11 @@ public class InnReservations {
                         sql_stm.append("%'");
                     } else {
                         sql_stm.append(LinksToInputs.get(arg2Option));
-                        if(arg2Option.equals("D")){
+                        if (arg2Option.equals("D")) {
                             sql_stm.append(" = '");
                             sql_stm.append(arg2);
                             sql_stm.append("'");
-                        }
-                        else {
+                        } else {
                             sql_stm.append(" = ");
                             sql_stm.append(arg2);
                         }
@@ -320,11 +331,11 @@ public class InnReservations {
                         sql_stm.append("%'");
                     } else {
                         sql_stm.append(LinksToInputs.get(arg1Option));
-                        if(arg1Option.equals("D")){
+                        if (arg1Option.equals("D")) {
                             sql_stm.append(" = '");
                             sql_stm.append(arg1);
                             sql_stm.append("'");
-                        }else {
+                        } else {
                             sql_stm.append(" = ");
                             sql_stm.append(arg1);
                         }
@@ -353,23 +364,21 @@ public class InnReservations {
                         sql_stm.append("%'");
                     } else {
                         sql_stm.append(LinksToInputs.get(arg2Option));
-                        if(arg2Option.equals("D")){
+                        if (arg2Option.equals("D")) {
                             sql_stm.append(" = '");
                             sql_stm.append(arg2);
                             sql_stm.append("'");
-                        }
-                        else {
+                        } else {
                             sql_stm.append(" = ");
                             sql_stm.append(arg2);
                         }
                     }
                     sql_stm.append(LinksToInputs.get(arg2Option));
-                    if(arg2Option.equals("D")){
+                    if (arg2Option.equals("D")) {
                         sql_stm.append(" = '");
                         sql_stm.append(arg2);
                         sql_stm.append("'");
-                    }
-                    else {
+                    } else {
                         sql_stm.append(" = ");
                         sql_stm.append(arg2);
                     }
@@ -399,12 +408,11 @@ public class InnReservations {
                         sql_stm.append("%'");
                     } else {
                         sql_stm.append(LinksToInputs.get(arg1Option));
-                        if(arg1Option.equals("D")){
+                        if (arg1Option.equals("D")) {
                             sql_stm.append(" = '");
                             sql_stm.append(arg1);
                             sql_stm.append("'");
-                        }
-                        else {
+                        } else {
                             sql_stm.append(" = ");
                             sql_stm.append(arg1);
                         }
@@ -435,7 +443,7 @@ public class InnReservations {
                     String FirstN = res_set.getString("FirstName");
                     int kidsNum = res_set.getInt("Kids");
                     int adultsNum = res_set.getInt("Adults");
-                    System.out.format("\n%-10s %-10s %-15s %-15s %-10s %-20s %-20s %-10s %-10s\n", codeForRoom,RoomString, CheckInString, CheckOutString, rateFloat, FirstN, LastN, kidsNum, adultsNum);
+                    System.out.format("\n%-10s %-10s %-15s %-15s %-10s %-20s %-20s %-10s %-10s\n", codeForRoom, RoomString, CheckInString, CheckOutString, rateFloat, FirstN, LastN, kidsNum, adultsNum);
 
                 }
 
@@ -445,5 +453,162 @@ public class InnReservations {
 
         }
     }
+
+    public static void reservationSearch(Scanner scanner, Connection conn) throws SQLException {
+        //•First name
+        //•Last name
+        //•A room code to indicate the specific room desired (or “Any” to indicate no preference)
+        //•A desired bed type (or “Any” to indicate no preference)
+        //•Begin date of stay•End date of stay
+        //•Number of children•Number of adults
+        String firstN;
+        String lastN;
+        String roomC;
+        String bedT;
+        String startD;
+        String endD;
+        String numOfChildren;
+        String numOfAdults;
+
+        ArrayList<PossibleEntries> possibilites = new ArrayList<PossibleEntries>();
+        //PossibleEntries[] possibilites = new PossibleEntries[20];
+
+        System.out.println("Please enter your first name:");
+        firstN = scanner.nextLine();
+        System.out.println("Please enter your last name:");
+        lastN = scanner.nextLine();
+        System.out.println("Please enter a desired roomCode, type 'ANY' if you have no preference");
+        roomC = scanner.nextLine();
+        System.out.println("Please enter the desired bed type, type 'ANY' if you have no preference");
+        bedT = scanner.nextLine();
+        System.out.println("Please enter the desired start date");
+        startD = scanner.nextLine();
+        System.out.println("Please enter the desired end date");
+        endD = scanner.nextLine();
+        System.out.println("Please enter the number of children");
+        numOfChildren = scanner.nextLine();
+        System.out.println("Please enter the number of adults");
+        numOfAdults = scanner.nextLine();
+
+        //occupied reseerations
+        // start before the startD and ends after the endD -> checkIn <= startD and checkOut >= endD
+        String dateCheck1 = "CheckIn <= '" + startD + "' and CheckOut >= '" + endD + "'";
+        //starts before and ends before the checkout -> checkIn <= startD and checkOut <= endD
+        String dateCheck2 = "CheckIn <= '" + startD + "' and CheckOut <= '" + endD + "'";
+        // starts after startD and before the endD -> checkIn >= startD and checkOut <= endD
+        String dateCheck3 = "CheckIn >= '" + startD + "' and CheckOut <= '" + endD + "'";
+        //starts after startD and end after the endD -> checkIn >= startD and checkOut >= endD
+        String dateCheck4 = "CheckIn >= '" + startD + "' and CheckOut >= '" + endD + "'";
+
+
+        StringBuilder sql_stm = new StringBuilder("with RoomOccupations2 as (select lab7_reservations.code, RoomName, Room, CheckIn, Checkout,");
+        sql_stm.append("Case when " + dateCheck1 + " and " + dateCheck2 + " and " + dateCheck3 + " and " + dateCheck4 + " ");
+        sql_stm.append("then 'Occupied' else 'Empty' ");
+        sql_stm.append("end as Status from lab7_reservations ");
+        sql_stm.append("join lab7_rooms on lab7_rooms.RoomCode = lab7_reservations.Room ");
+        sql_stm.append("group by code, RoomName), ");
+        sql_stm.append("OccupiedCodes2 as (select Room from RoomOccupations2 ");
+        sql_stm.append("where Status = 'Occupied' order by Room desc), \n ");
+        sql_stm.append("UnoccupiedOnRange as (select * from RoomOccupations2 ");
+        sql_stm.append("where Room not in (select * from OccupiedCodes2)), \n");
+        sql_stm.append("distinctRooms as (select distinct Room from UnoccupiedOnRange) \n"); // unoccupied rooms
+
+        //System.out.printlnun(sql_stm);
+        boolean doneAlready = false;
+
+        //•A room code to indicate the specific room desired (or “Any” to indicate no preference)
+        //•A desired bed type (or “Any” to indicate no preference)
+        //•Begin date of stay•End date of stay
+        //•Number of children•Number of adults
+        int totalOcc = Integer.parseInt(numOfAdults) + Integer.parseInt(numOfChildren);
+        String total = String.valueOf(totalOcc);
+        StringBuilder allOptions = new StringBuilder(sql_stm);
+        if (!roomC.equals("ANY") && !bedT.equals("ANY")) {
+            allOptions.append("select * from lab7_rooms where RoomCode in (select * from distinctRooms) and ");
+            allOptions.append("RoomCode = '" + roomC + "' and ");
+            allOptions.append("bedType = '" + bedT + "' and ");
+            allOptions.append("maxOcc >= " + total);
+
+        }
+        else if(roomC.equals("ANY") && !bedT.equals("ANY")){
+            allOptions.append("select * from lab7_rooms where RoomCode in (select * from distinctRooms) and ");
+            allOptions.append("bedType = '" + bedT + "' and ");
+            allOptions.append("maxOcc >= " + total);
+        }
+        else if(!roomC.equals("ANY") && bedT.equals("ANY")){
+            allOptions.append("select * from lab7_rooms where RoomCode in (select * from distinctRooms) and ");
+            allOptions.append("RoomCode = '" + roomC + "' and ");
+            allOptions.append("maxOcc >= " + total);
+        }
+        else {
+            doneAlready = true;
+            allOptions.append("select * from lab7_rooms where RoomCode in (select * from distinctRooms) and ");
+            allOptions.append("maxOcc >= " + total);
+        }
+
+        String sql = allOptions.toString();
+
+        //executing SQL statement.
+        try (PreparedStatement prep_stm = conn.prepareStatement(sql)) {
+            try (ResultSet res_set = prep_stm.executeQuery()) {
+                System.out.format("\n%-15s %-30s %-15s %-15s %-15s %-20s %-20s\n", "Room Code", "Room Name", "Beds", "BedType", "maxOcc", "Base Price", "Decor");
+
+                while (res_set.next()) {
+                    String codeForRoom = res_set.getString("RoomCode");
+                    String RoomString = res_set.getString("RoomName");
+                    String Beds = res_set.getString("Beds");
+                    String BedTy = res_set.getString("bedType");
+                    int maxO = res_set.getInt("MaxOcc");
+                    int basePrice  = res_set.getInt("basePrice");
+                    String decor = res_set.getString("decor");
+                    System.out.format("\n%-15s %-30s %-15s %-15s %-15s %-20s %-20s\n", codeForRoom, RoomString, Beds, BedTy, maxO, basePrice, decor);
+                    PossibleEntries newP = new PossibleEntries(codeForRoom, RoomString, basePrice);
+                    possibilites.add(newP);
+
+
+                    //possibilites.add(new PossibleEntries)
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(!doneAlready){
+            StringBuilder RoomAva = new StringBuilder(sql_stm);
+            RoomAva.append("select * from lab7_rooms where RoomCode in (select * from distinctRooms) and ");
+            RoomAva.append("maxOcc >= " + total);
+
+            String minSql = RoomAva.toString();
+
+            //executing SQL statement.
+            try (PreparedStatement prep_stm = conn.prepareStatement(minSql)) {
+                try (ResultSet res_set = prep_stm.executeQuery()) {
+                    System.out.format("\n%-15s %-30s %-15s %-15s %-15s %-20s %-20s\n", "Room Code", "Room Name", "Beds", "BedType", "maxOcc", "Base Price", "Decor");
+
+                    while (res_set.next()) {
+                        String codeForRoom = res_set.getString("RoomCode");
+                        String RoomString = res_set.getString("RoomName");
+                        String Beds = res_set.getString("Beds");
+                        String BedTy = res_set.getString("bedType");
+                        int maxO = res_set.getInt("MaxOcc");
+                        int basePrice = res_set.getInt("basePrice");
+                        String decor = res_set.getString("decor");
+                        System.out.format("\n%-15s %-30s %-15s %-15s %-15s %-20s %-20s\n", codeForRoom, RoomString, Beds, BedTy, maxO, basePrice, decor);
+                        PossibleEntries newP = new PossibleEntries(codeForRoom, RoomString, basePrice);
+                        possibilites.add(newP);
+
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
 }
+
+
 
