3c0ab6f40a5ac1be6af0be61dd2d31de92cc189d
jennifer-vazquez <vazquez.jennifer5@gmail.com>
2022-06-10 12:35:55 -0700

    rev done

--- Lab7/src/InnReservations.java
+++ Lab7/src/InnReservations.java
@@ -1,23 +1,27 @@
 
 import java.sql.*;
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 import java.util.Scanner;
 import java.time.LocalDate;
+import java.util.concurrent.TimeUnit;
 
 public class InnReservations {
     public static class PossibleEntries {
-        public String RoomCode, RoomN;
+        public String RoomCode, RoomN, bedType;
         public int rate;
 
             // constructor
-            public PossibleEntries(String RC, String RN, int Ra) {
+            public PossibleEntries(String RC, String RN, int Ra, String BT) {
                 this.RoomCode = RC;
                 this.RoomN = RN;
                 this.rate = Ra;
+                this.bedType = BT;
             }
     }
 
@@ -57,6 +61,9 @@ public class InnReservations {
                 }
                 if (optionSelected.equals("5")) {
                     detailedReservation(myScan, conn);
+                }
+                if(optionSelected.equals("6")){
+                    Revenue(conn);
                 }
             }
         }
@@ -482,9 +489,9 @@ public class InnReservations {
         roomC = scanner.nextLine();
         System.out.println("Please enter the desired bed type, type 'ANY' if you have no preference");
         bedT = scanner.nextLine();
-        System.out.println("Please enter the desired start date");
+        System.out.println("Please enter the desired start date (Format must be YYYY-MM-DD)");
         startD = scanner.nextLine();
-        System.out.println("Please enter the desired end date");
+        System.out.println("Please enter the desired end date (Format must be YYYY-MM-DD)");
         endD = scanner.nextLine();
         System.out.println("Please enter the number of children");
         numOfChildren = scanner.nextLine();
@@ -550,7 +557,7 @@ public class InnReservations {
 
                 int counterToCheck = 0;
                 while (res_set.next()) {
-                    if(counterToCheck == 0){
+                    if (counterToCheck == 0) {
                         System.out.println("All Options considered:");
                     }
                     String codeForRoom = res_set.getString("RoomCode");
@@ -561,14 +568,14 @@ public class InnReservations {
                     int basePrice = res_set.getInt("basePrice");
                     String decor = res_set.getString("decor");
                     System.out.format("\n%-15s %-15s %-30s %-15s %-15s %-15s %-20s %-20s\n", counter, codeForRoom, RoomString, Beds, BedTy, maxO, basePrice, decor);
-                    PossibleEntries newP = new PossibleEntries(codeForRoom, RoomString, basePrice);
+                    PossibleEntries newP = new PossibleEntries(codeForRoom, RoomString, basePrice, BedTy);
                     possibilites.add(newP);
                     counter++;
                     counterToCheck++;
 
                     //possibilites.add(new PossibleEntries)
                 }
-                if(counterToCheck == 0){
+                if (counterToCheck == 0) {
                     System.out.println("Nothing is available based on your requests:");
                 }
 
@@ -591,7 +598,7 @@ public class InnReservations {
                     //System.out.format("\n%-10s%-15s %-30s %-15s %-15s %-15s %-20s %-20s\n", "Room Code", "Room Name", "Beds", "BedType", "maxOcc", "Base Price", "Decor");
                     System.out.println("Only dates and availability considered:");
                     while (res_set.next()) {
-                        if(counter < 6) {
+                        if (counter < 6) {
                             String codeForRoom = res_set.getString("RoomCode");
                             String RoomString = res_set.getString("RoomName");
                             String Beds = res_set.getString("Beds");
@@ -600,7 +607,7 @@ public class InnReservations {
                             int basePrice = res_set.getInt("basePrice");
                             String decor = res_set.getString("decor");
                             System.out.format("\n%-15s %-15s %-30s %-15s %-15s %-15s %-20s %-20s\n", counter, codeForRoom, RoomString, Beds, BedTy, maxO, basePrice, decor);
-                            PossibleEntries newP = new PossibleEntries(codeForRoom, RoomString, basePrice);
+                            PossibleEntries newP = new PossibleEntries(codeForRoom, RoomString, basePrice, BedTy);
                             possibilites.add(newP);
                             counter++;
                         }
@@ -612,45 +619,171 @@ public class InnReservations {
                 }
             }
         }
-        System.out.println("Would you like to book a reservation? Y/N");
-        String answer = scanner.nextLine();
-        if (answer.equals("Y")) {
-            System.out.println("Select the option you would like:");
-            int optionSelected = scanner.nextInt();
-            PossibleEntries option = possibilites.get(optionSelected - 1);
-            try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO lab7_reservations (CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);")) {
-             int reserv_code = 0;
-                try (Statement stm2 = conn.createStatement()) {
-                    //Get max code from reservations to generate potential future next code.
-                    String sql2 = "SELECT max(CODE) as CODE from lab7_reservations;";
-                    ResultSet res_set2 = stm2.executeQuery(sql2);
+        if (possibilites.size() == 0) {
+            System.out.println("No suitable rooms available");
+        } else {
+            System.out.println("Would you like to book a reservation? Y/N");
+            String answer = scanner.nextLine();
+            if (answer.equals("Y")) {
+                System.out.println("Select the option you would like:");
+                int optionSelected = scanner.nextInt();
+                PossibleEntries option = possibilites.get(optionSelected - 1);
+                try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO lab7_reservations (CODE, Room, CheckIn, CheckOut, Rate, LastName, FirstName, Adults, Kids) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);")) {
+                    int reserv_code = 0;
+                    try (Statement stm2 = conn.createStatement()) {
+                        //Get max code from reservations to generate potential future next code.
+                        String sql2 = "SELECT max(CODE) as CODE from lab7_reservations;";
+                        ResultSet res_set2 = stm2.executeQuery(sql2);
 
-                    while (res_set2.next()) {
-                        String code = res_set2.getString("CODE");
-                        reserv_code = Integer.parseInt(code) + 1;
+                        while (res_set2.next()) {
+                            String code = res_set2.getString("CODE");
+                            reserv_code = Integer.parseInt(code) + 1;
+                        }
+                        pstmt.setInt(1, reserv_code);
+                        pstmt.setString(2, option.RoomCode);
+                        pstmt.setDate(3, Date.valueOf(startD));
+                        pstmt.setDate(4, Date.valueOf(endD));
+                        pstmt.setDouble(5, option.rate);
+                        pstmt.setString(6, lastN);
+                        pstmt.setString(7, firstN);
+                        pstmt.setInt(8, Integer.parseInt(numOfAdults));
+                        pstmt.setInt(9, Integer.parseInt(numOfChildren));
+
+                        pstmt.executeUpdate();
+                        conn.commit();
+                        System.out.println("Successful reservation with:");
+                        String date1 = startD.substring(5, 7) + "/" + startD.substring(8,10)+ "/" + startD.substring(0,4);
+                        String date2 = endD.substring(5, 7) + "/" + endD.substring(8,10)+ "/" + endD.substring(0,4);
+                       // System.out.println(date1);
+                                //yyyy-mm-dd
+                        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH);
+
+                        long diffInMillies = Math.abs(sdf.parse(date1).getTime() - sdf.parse(date2).getTime());
+                        long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+                        int weekdays = getWorkingDaysBetweenTwoDates(sdf.parse(date1), sdf.parse(date2));
+                        System.out.println(weekdays);
+                        System.out.println("First Name: "+ firstN);
+                        System.out.println("Last Name: "+ lastN);
+                        System.out.println("Room Code: "+ option.RoomCode);
+                        System.out.println("Room Name: "+ option.RoomN);
+                        System.out.println("Bed Type: "+ option.bedType);
+                        System.out.println("Start Date: " + startD);
+                        System.out.println("End Date: " + endD);
+                        System.out.println("Number of Adults: "+ numOfAdults);
+                        System.out.println("Number of Children: "+ numOfChildren);
+                        System.out.print("Total Cost: ");
+                        System.out.format("%-2s\n", option.rate * weekdays + option.rate * 1.10 * (diff - weekdays));
+                        /*
+                        First name,
+                        last name•
+                        Room code,
+                         room name,
+                         bed type•
+                         Begin and end date of stay•
+                         Number of adults•
+                         Number of children•
+                         Total cost of stay, based on a sum of the following:–Number of weekdays multipled by room base rate–Number of weekend days multiplied by 110% of the room base rate
+                         */
+                        scanner.nextLine();
+                    } catch (SQLException | ParseException e) {
+                        e.printStackTrace();
                     }
-                    pstmt.setInt(1, reserv_code);
-                    pstmt.setString(2, option.RoomCode);
-                    pstmt.setDate(3, Date.valueOf(startD));
-                    pstmt.setDate(4, Date.valueOf(endD));
-                    pstmt.setDouble(5, option.rate);
-                    pstmt.setString(6, lastN);
-                    pstmt.setString(7, firstN);
-                    pstmt.setInt(8, Integer.parseInt(numOfAdults));
-                    pstmt.setInt(9, Integer.parseInt(numOfChildren));
-
-                    pstmt.executeUpdate();
-                    conn.commit();
-                    System.out.println("Successful reservation");
-                    scanner.nextLine();
-                }
-                catch (SQLException e) {
-                    e.printStackTrace();
                 }
             }
         }
     }
+
+
+    public static int getWorkingDaysBetweenTwoDates(java.util.Date startDate, java.util.Date endDate) {
+        Calendar startCal = Calendar.getInstance();
+        startCal.setTime(startDate);
+
+        Calendar endCal = Calendar.getInstance();
+        endCal.setTime(endDate);
+
+        int workDays = 0;
+
+        //Return 0 if start and end are the same
+        if (startCal.getTimeInMillis() == endCal.getTimeInMillis()) {
+            return 0;
+        }
+
+        if (startCal.getTimeInMillis() > endCal.getTimeInMillis()) {
+            startCal.setTime(endDate);
+            endCal.setTime(startDate);
+        }
+
+        do {
+            //excluding start date
+            startCal.add(Calendar.DAY_OF_MONTH, 1);
+            if (startCal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && startCal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+                ++workDays;
+            }
+        } while (startCal.getTimeInMillis() < endCal.getTimeInMillis()); //excluding end date
+
+        return workDays;
     }
+
+
+    private static void Revenue(Connection conn) {
+        System.out.println("\nMonth-By-Month revenue overview:\n");
+        StringBuilder sqlstat = new StringBuilder();
+        sqlstat.append("with rev as (select Room, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 1 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as January, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 2 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as February, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 3 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as March, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 4 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as April, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 5 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as May, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 6 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as June, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 7 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as July, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 8 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as August, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 9 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as September, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 10 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as October, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 11 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as November, ");
+        sqlstat.append("ABS(round(sum(case when month(Checkout) = 12 then datediff(Checkout, CheckIn) *-1 * rate else 0 end),0)) as December, ");
+        sqlstat.append("ABS(round(sum(datediff(Checkout, Checkin) *-1 * rate),0)) as Annual \n");
+        sqlstat.append("from lab7_reservations \n");
+        sqlstat.append("group by Room)\n");
+        sqlstat.append("select Room, January, February, March, April, May, June, July, August, September, October, November, December, Annual ");
+        sqlstat.append("from rev ");
+        sqlstat.append("union select 'Total', sum(January), sum(February), sum(March), sum(April), sum(May), sum(June), sum(July), sum(August),sum(September), sum(October), sum(November), sum(December), sum(Annual) from rev");
+
+        System.out.println(sqlstat);
+        String sql = sqlstat.toString();
+        try(Statement stm = conn.createStatement())
+        {
+            ResultSet res_set = stm.executeQuery(sql);
+
+            System.out.format("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", "Room(s)", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "Anual");
+            System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            while (res_set.next())
+            {
+                String room = res_set.getString("Room");
+                String january = res_set.getString("January");
+                String february = res_set.getString("February");
+                String march = res_set.getString("March");
+                String april = res_set.getString("April");
+                String may = res_set.getString("May");
+                String june = res_set.getString("June");
+                String july = res_set.getString("July");
+                String august = res_set.getString("August");
+                String september = res_set.getString("September");
+                String october = res_set.getString("October");
+                String november = res_set.getString("November");
+                String december = res_set.getString("December");
+                String annual = res_set.getString("Annual");
+
+                System.out.format("%-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", room + ":", "$" + january, "$" + february, "$" + march, "$" + april, "$" + may, "$" + june, "$" + july, "$" + august, "$" + september, "$" + october, "$" + november, "$" + december, "$" + annual);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println("\n" + e);
+        }
+
+    }
+
+}
 
 
 
